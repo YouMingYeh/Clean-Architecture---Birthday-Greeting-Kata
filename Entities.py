f190947655d6e054523b31c36f2f6d6e064ba259
YouMingYeh <ym911216@gmail.com>
2023-12-01 12:19:29 +0800

    feat: Define member entity

---
+++ Entities.py
@@ -0,0 +1,8 @@
+# 1. Define member entity
+class Member:
+    def __init__(self, first_name, last_name, gender, date_of_birth, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.gender = gender
+        self.date_of_birth = date_of_birth
+        self.email = email
