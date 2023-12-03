b59d0c2f2d8840b5260351695742e450acd5acfe
YouMingYeh <b10705052@ntu.edu.tw>
2023-12-03 23:59:48 +0800

    fix: Create init table method for MongodbRepo

--- Repositories/MongoDBMemberRepository.py
+++ Repositories/MongoDBMemberRepository.py
@@ -1,6 +1,6 @@
 from Entities import Member
 from typing import List
-from datetime import date
+from datetime import date, datetime
 from Repositories.Member import MemberRepository
 from pymongo.mongo_client import MongoClient
 from dotenv import load_dotenv
@@ -16,12 +16,59 @@ class MongoDBMemberRepository(MemberRepository):
         self.db = self.client["CA"]
         self.collection = self.db["member"]
 
+        self.members = [
+            Member(
+                first_name="Robert",
+                last_name="Yen",
+                gender="Male",
+                date_of_birth=date(1985, 8, 8),
+                email="robert.yen@linecorp.com",
+            ),
+            Member(
+                first_name="Cid",
+                last_name="Change",
+                gender="Male",
+                date_of_birth=date(1990, 10, 10),
+                email="cid.change@linecorp.com",
+            ),
+            Member(
+                first_name="Miki",
+                last_name="Lai",
+                gender="Female",
+                date_of_birth=date(1993, 4, 5),
+                email="miki.lai@linecorp.com",
+            ),
+            Member(
+                first_name="Sherry",
+                last_name="Chen",
+                gender="Female",
+                date_of_birth=date(1993, 8, 8),
+                email="sherry.lai@linecorp.com",
+            ),
+            Member(
+                first_name="Peter",
+                last_name="Wang",
+                gender="Male",
+                date_of_birth=date(1950, 12, 22),
+                email="peter.wang@linecorp.com",
+            ),
+        ]
+
     def add(self, member: Member):
-        # TODO: Implement MongoDB Insert
-        self.collection.insert_one(member.__dict__)
+        member_dict = member.__dict__
+        member_dict["date_of_birth"] = datetime.combine(member_dict["date_of_birth"], datetime.min.time())
+        self.collection.insert_one(member_dict)
+
+    def init_table(self):
+        members_dict = [member.__dict__ for member in self.members]
+        for member_dict in members_dict:
+            member_dict["date_of_birth"] = datetime.combine(member_dict["date_of_birth"], datetime.min.time())
+        self.collection.insert_many(members_dict)
+
+    def drop_table(self):
+        self.collection.drop()
 
     def get_all(self):
-        # TODO: Implement MongoDB Select
         members = self.collection.find()
         members = [
             Member(
@@ -36,7 +83,6 @@ class MongoDBMemberRepository(MemberRepository):
         return members
 
     def get_members_with_tody_birthday(self):
-        # TODO: Implement MongoDB Select
         today = self.today_date.strftime("%m-%d")
         members = self.collection.find({"date_to_birth": today})
         members = [
